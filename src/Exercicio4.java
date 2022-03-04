ad956063c2d6f26fba891dbdebbcb0f75787c330
jeankrausejean <jeankrausejean@gmail.com>
2022-03-04 13:05:26 -0300

    Exercicio 4 corrigido

--- src/Exercicio4.java
+++ src/Exercicio4.java
@@ -1,23 +1,25 @@
 
 public class Exercicio4 {
 	
-		//Precisa corrigir, não esta correto,... ainda...
+		//Corrigido!!!
 	
 	public static void main(String[] args) {
 		
 		int vetor[] =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	     
-		  System.out.println("Ordem original:\n");
-		 
-		  for(int i = 0; i < vetor.length; i++){
-		    System.out.print(vetor[i] + "  ");
-		  }
-		 
-		  System.out.println("\n\nOrdem o:\n");
-		 
-		  for(int i = vetor.length - 1; i >= 0; i--){
-		    System.out.print(vetor[i] + "  ");
-		  }
+		//Primeira solução
+		
+		/*for (int valor : vetor) {
+			System.out.println(valor + "" + ((valor - vetor.length) - 1));
+		}*/
+		
+		//segunda solução
+		
+		int controle = vetor.length -1;
+		for (int i = 0; i < vetor.length; i++) {
+		System.out.println( vetor[i] + " - " + vetor[controle]);
+		controle--;
+		}
 	}
 
 }
